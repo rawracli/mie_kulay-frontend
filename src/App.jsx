0e23fdb28b6ae277e269bfd9374dc5352f60ce3f
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-28 21:22:29 +0700

    Menambah routing untuk admin page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import MainLayout from "./layouts/MainLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Dashboard from "./pages/Admin/Dashboard/Dashboard";
+import Pemesanan from "./pages/Admin/Pemesanan/Pemesanan";
+import Stok from "./pages/Admin/Stok/Stok";
+import LogAktivitas from "./pages/Admin/LogAktivitas/LogAktivitas";
 
 function App() {
   return (
@@ -11,6 +16,12 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />}></Route>
+        </Route>
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/pemesanan" element={<Pemesanan />}></Route>
+          <Route path="/stok" element={<Stok />}></Route>
+          <Route path="/log-aktivitas" element={<LogAktivitas />}></Route>
         </Route>
           <Route path="/login" element={<Home />}></Route>
           <Route path="/register" element={<Home />}></Route>
