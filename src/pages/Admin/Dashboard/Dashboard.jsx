80882671e31ed7ce45463d3a53d9ee398af1a250
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-08-28 21:08:00 +0700

    Menambah struktur halaman

---
+++ src/pages/Admin/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="bg-gray-500 h-[115vh] w-full flex items-center justify-center">
+      <p className="text-8xl">DASHBOARD PAGE</p>
+    </div>
+  );
+}
+
+export default Dashboard;
