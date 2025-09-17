ad172eace0db0d58b07962273e7628c99e8d926f
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-09-17 11:35:32 +0700

    Mengubah img3 menjadi embed youtube

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,7 +22,6 @@ import logo from "../../assets/logoTransparant.svg";
 import Menu from "./Section/Menu";
 import img1 from "../../assets/Home/img1.png";
 import img2 from "../../assets/Home/img2.png";
-import img3 from "../../assets/Home/img3.png";
 import { Carousel } from "react-responsive-3d-carousel";
 import "react-responsive-3d-carousel/dist/styles.css";
 
@@ -30,54 +29,72 @@ export default function HeroSection() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const slides = [
-    { src: img1, link: "https://example.com/page1" },
-    { src: img2, link: "https://example.com/page2" },
-    { src: img3, link: "https://example.com/page3" },
+    { type: "youtube", src: "https://www.youtube.com/embed/mnHhVOZiCpw?si=L_uhf6-FnrEwZEAg"},
+    { type: "image", src: img1, link: "https://vt.tiktok.com/ZSDBbymHr/" },
+    { type: "image", src: img2, link: "https://vt.tiktok.com/ZSDBgj1GM/" },
   ];
-
   const items = slides.map((item, index) => (
     <div
       key={index}
       className="relative w-full h-full cursor-pointer"
       onClick={() => {
-        if (currentIndex === index) {
+        if (currentIndex === index && item.type === "image") {
           window.open(item.link, "_blank");
         }
       }}
     >
-      <img
-        src={item.src}
-        alt=""
-        className="rounded-[20px] border-white border-4 w-full h-full object-cover"
-      />
+      {item.type === "youtube" ? (
+        <iframe
+          width="100%"
+          height="100%"
+          src={item.src}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="rounded-[20px] border-white border-4"
+        ></iframe>
+      ) : (
+        <img
+          src={item.src}
+          alt=""
+          className="rounded-[20px] border-white border-4 w-full h-full object-cover"
+        />
+      )}
 
-      {/* Overlay hitam kalau bukan current */}
-      <div
-        className={`absolute z-10 inset-0 rounded-[20px] transition duration-300 ${
-          currentIndex !== index ? "bg-black/50" : "bg-black/0"
-        }`}
-      ></div>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-[98px] h-[96px] bg-black/43 rounded-full flex items-center justify-center">
-        <svg
-          width="36"
-          height="43"
-          className="ml-2"
-          viewBox="0 0 36 43"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1.33398 1.75L34.2507 21.5L1.33398 41.25V1.75Z"
-            fill="white"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </div>
+      {/* Overlay untuk image saja */}
+      {item.type === "image" && (
+        <>
+          <div
+            className={`absolute z-10 inset-0 rounded-[20px] transition duration-300 ${
+              currentIndex !== index ? "bg-black/50" : "bg-black/0"
+            }`}
+          ></div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-[98px] h-[96px] bg-black/43 rounded-full flex items-center justify-center">
+            <svg
+              width="36"
+              height="43"
+              className="ml-2"
+              viewBox="0 0 36 43"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.33398 1.75L34.2507 21.5L1.33398 41.25V1.75Z"
+                fill="white"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </>
+      )}
     </div>
   ));
+
   return (
     <>
       <div className="overflow-x-hidden">
@@ -222,6 +239,7 @@ export default function HeroSection() {
             />
             <Menu />
           </div>
+          {/* MAP SECTION */}
           <div className="relative min-h-1/2">
             <div className="flex items-center justify-center pt-[125px]">
               <h2 className="font-boogaloo text-[60px]">
@@ -247,6 +265,7 @@ export default function HeroSection() {
             </div>
           </div>
         </div>
+        {/* CAROUSEL VIDEO */}
         <div className="relative min-h-svh max-h-[720px] flex">
           <div className="flex-1 bg-[#FFB300]">
             <h2 className="text-[60px] font-boogaloo text-end mr-[12px] pt-[57px]">
