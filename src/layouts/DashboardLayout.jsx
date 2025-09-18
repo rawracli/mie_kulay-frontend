ac98428f202a4adfbd0ad586fae311e9be41771a
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-09-18 21:37:04 +0700

    Mengubah navbar dan sidebar menjadi responsive

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Admin/Navbar";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Admin/Sidebar";
 function MainLayout() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="h-screen flex">
-      <Sidebar />
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div className="flex flex-col flex-1 max-h-screen">
-        <Navbar />
+        <Navbar setIsOpen={setIsOpen}/>
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
