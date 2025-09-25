7b210a3ca069ead1e961a5a0d22c64b018c054d1
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-09-25 21:48:57 +0700

    hotfix: Mengubah height menggunakan svh
    memperbaiki masalah sidebar dan page tidak pas fullscreen di mobile device

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -5,9 +5,9 @@ import Sidebar from "../components/Admin/Sidebar";
 function MainLayout() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="h-screen flex">
+    <div className="h-svh flex">
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}/>
-      <div className="flex flex-col flex-1 max-h-screen">
+      <div className="flex flex-col flex-1 max-h-svh">
         <Navbar setIsOpen={setIsOpen}/>
         <main className="flex-1 overflow-y-auto">
           <Outlet />
