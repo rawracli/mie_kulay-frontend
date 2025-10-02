18eef5e0b32c079c52b37e386c1caa430fb9b824
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-10-02 18:43:20 +0700

    Menambahkan animasi di hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,16 @@ function Hero({ text }) {
       <img
         src={Line}
         alt="garis melengkung"
+        data-aos="fade-up" data-aos-duration="1500"
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 w-[100%] h-auto"
       />
       <img
         src={Line}
         alt="garis melengkung"
+        data-aos="fade-up" data-aos-duration="2000" data-aos-delay="200"
         className="absolute top-[35%] left-1/2 -translate-x-1/2 -translate-y-1/3 w-[100%] h-auto"
       />
-      <div className="relative flex items-center justify-center z-10 bg-[#D72629] text-white rounded-full w-[201px] h-[45px] sm:w-[381px] sm:h-[72px] text-center">
+      <div className="relative flex items-center justify-center z-10 bg-[#D72629] text-white rounded-full w-[201px] h-[45px] sm:w-[381px] sm:h-[72px] text-center" data-aos="fade-up" data-aos-duration="800" data-aos-delay="100">
         <h2 className="text-[36px] sm:text-[64px] font-boogaloo font-bold pb-1">{text}</h2>
       </div>
     </div>
