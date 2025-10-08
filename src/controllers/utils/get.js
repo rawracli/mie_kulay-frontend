b55248bd2cab271dd9db7c308b98eb2e1d190bdd
RafliIsLife <149589644+RaflIsLife@users.noreply.github.com>
2025-10-08 13:40:11 +0700

    fix: typo

--- src/controllers/utils/get.js
+++ src/controllers/utils/get.js
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie';
-const getWEB = `${import.meta.env.VITE_API_URL_IMAGE}`;
+const getWEB = `${import.meta.env.VITE_API_URL}`;
 
 export const gctks = async () => {
   await fetch(`${getWEB}/sanctum/csrf-cookie`, {
