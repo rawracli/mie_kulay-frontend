8a2bbfe5ab0897b72695bfcbe5ae4facf480b00c
ghassarsa <ppziensxhu@gmail.com>
2025-10-17 23:27:34 +0700

    fix unauthorized pemesanan

--- src/controllers/Pemesanan.js
+++ src/controllers/Pemesanan.js
@@ -1,9 +1,17 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+const getToken = () => {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("User belum login");
+  return token;
+};
+
 const getPemesanan = async () => {
+  const token = getToken();
   const res = await fetch(`${API_URL}/pesanan`, {
     headers: {
       "Accept": "application/json",
+      "Authorization": `Bearer ${token}`,
     },
   });
 
